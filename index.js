96e438f0d93b7953b612b1f2fb8c63eb46302e0d
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-08 12:37:26 -0700

    installed cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.use(cors());
 app.use(express.json());
+
 app.use(`/api`, (_, res) => {
   res.json({ greet: "Hello" });
 });
